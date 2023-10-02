3e7dba4b9eb91e2c436b112fa832cba7c494451f
Kongsted <mikkel.kongsted@siemensgamesa.com>
2023-10-02 14:01:48 +0200

    create gameloop

--- src/Board.ts
+++ src/Board.ts
@@ -1,4 +1,6 @@
+import { Coordinate } from "./Coordinate";
 import { Graphics } from "./Graphics";
+import { Piece } from "./Pieces";
 import { Block } from "./block";
 import { COLS, ROWS } from "./constants";
 
@@ -7,16 +9,49 @@ export class Board {
     constructor(private graphics: Graphics) {
         this.grid = this.getEmptyBoard();
         console.table(this.grid);
+        this.draw();
+    }
+
+    consoleTable() {
+        console.table(this.grid);
+    }
+
+    draw() {
+        this.graphics.clear();
         this.graphics.drawBoard(this.grid);
     }
 
     getEmptyBoard(): Block[][] {
-        const emptyGrid = Array.from({ length: ROWS }, () => Array(COLS));
+        const emptyGrid = Array.from({ length: COLS }, () => Array(ROWS));
         for (let i = 0; i < emptyGrid.length; i++) {
             for (let j = 0; j < emptyGrid[i].length; j++) {
                 emptyGrid[i][j] = { value: 0, color: "" };
             }
         }
         return emptyGrid;
+    }
+
+    setPieceInPos(piece: Piece, pos: Coordinate) {
+        for (let i = 0; i < piece.shape.length; i++) {
+            for (let j = 0; j < piece.shape[i].length; j++) {
+                this.grid[pos.x + j][pos.y + i].value = piece.shape[i][j].value;
+                this.grid[pos.x + j][pos.y + i].color = piece.shape[i][j].color;
+            }
+        }
+    }
+    removePieceInPos(piece: Piece, pos: Coordinate) {
+        for (let i = 0; i < piece.shape.length; i++) {
+            for (let j = 0; j < piece.shape[i].length; j++) {
+                if (piece.shape[i][j].value == 1) {
+                    this.grid[pos.x + j][pos.y + i].value = 0;
+                    this.grid[pos.x + j][pos.y + i].color = "";
+                }
+            }
+        }
+    }
+
+    movePiece(piece: Piece, from: Coordinate, to: Coordinate) {
+        this.removePieceInPos(piece, from);
+        this.setPieceInPos(piece, to);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,33 @@
-import { Board } from './Board'
-import { FigureOrder } from './FigureOrder'
-import { Graphics } from './Graphics'
-import { I } from './Pieces'
-import {ROWS, COLS, BLOCKSIZE} from './constants'
-import './style.css'
+import { Board } from "./Board";
+import { FigureOrder } from "./FigureOrder";
+import { GameLoop } from "./GameLoop";
+import { Graphics } from "./Graphics";
+import { I } from "./Pieces";
+import { ROWS, COLS, BLOCKSIZE } from "./constants";
+import "./style.css";
 
 function main() {
-    const canvas = document.querySelector<HTMLCanvasElement>(`#game`)!
-    canvas.width = COLS*BLOCKSIZE
-    canvas.height = ROWS*BLOCKSIZE
-    const board = new Board(new Graphics(canvas))
-    const piece = new I()
+    const canvas = document.querySelector<HTMLCanvasElement>(`#game`)!;
+    canvas.width = COLS * BLOCKSIZE;
+    canvas.height = ROWS * BLOCKSIZE;
+    const board = new Board(new Graphics(canvas));
+    const piece = new I();
 
-    
-    
-    const figureOrder = new FigureOrder()
-    
+    const figureOrder = new FigureOrder();
+    const startPiecePos = { x: 3, y: 0 };
+    let currentPos = startPiecePos;
+
+    function gameIteration(deltaTime: number) {
+        board.movePiece(piece, currentPos, {
+            x: currentPos.x,
+            y: currentPos.y + 1,
+        });
+        board.draw();
+        currentPos.y++;
+    }
+
+    let currentFPS = 1;
+    const gameLoop = new GameLoop(currentFPS, gameIteration);
 }
 
-main()
+main();
