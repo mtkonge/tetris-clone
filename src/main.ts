d9298a916c0b489ebbb17c132bd11e93b97d5b60
Kongsted <mikkel.kongsted@siemensgamesa.com>
2023-10-18 16:19:07 +0200

    create ui for hold, next, and score feature

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { ROWS, COLS, BLOCKSIZE } from "./constants";
 import "./style.css";
 
 function main() {
-    const canvas = document.querySelector<HTMLCanvasElement>(`#game`)!;
-    canvas.width = COLS * BLOCKSIZE;
-    canvas.height = ROWS * BLOCKSIZE;
-    new Game(canvas);
+    const mainCanvas = document.querySelector<HTMLCanvasElement>(`#game`)!;
+    mainCanvas.width = COLS * BLOCKSIZE;
+    mainCanvas.height = ROWS * BLOCKSIZE;
+    const holdCanvas = document.querySelector<HTMLCanvasElement>(`#hold`)!;
+    holdCanvas.width = (4 + 1) * BLOCKSIZE;
+    holdCanvas.height = (2 + 1) * BLOCKSIZE;
+    const nextCanvas = document.querySelector<HTMLCanvasElement>(`#next`)!;
+    nextCanvas.width = (4 + 1) * BLOCKSIZE;
+    nextCanvas.height = (6 + 2) * BLOCKSIZE;
+
+    new Game(mainCanvas);
 }
 
 main();
